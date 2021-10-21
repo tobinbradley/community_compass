080cdb9d02ffb9906cf9a47f4ac6e41142f9e437
tobinbradley <tobin.bradley@gmail.com>
2021-10-21 17:44:21 -0400

    fix dnd

--- src/lib/grid.svelte
+++ src/lib/grid.svelte
@@ -1,35 +1,29 @@
 <script>
-import Card from '$lib/card.svelte'
-import { dndzone } from 'svelte-dnd-action'
-import { activeMetrics } from '$lib/store'
+  import Card from "$lib/card.svelte";
+  import { dndzone } from "svelte-dnd-action";
+  import { activeMetrics } from "$lib/store";
 
-//$: items = $activeMetrics
+  $: items = $activeMetrics;
 
-let items = [
-        {id: 1, name: "item1"},
-        {id: 2, name: "item2"},
-        {id: 3, name: "item3"},
-        {id: 4, name: "item4"}
-    ];
-
-function handleSort(e) {
-    console.log(e)
-    items = e.detail.items
-    //$activeMetrics = e.detail.items;
-    //$activeMetrics = $activeMetrics
-}
-
-
+  function handleSort(e) {
+    $activeMetrics = e.detail.items;
+    $activeMetrics = $activeMetrics;
+  }
 </script>
 
-
 <div class="flex-grow px-4 mt-8">
-    <div class="container mx-auto">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6" use:dndzone="{{items}}" on:consider={handleSort} on:finalize={handleSort}>
-            {#each items as item(item.id)}
-            <Card metric={item} />
-            {/each}  
+  <div class="container mx-auto">
+    <div
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
+      use:dndzone={{ items }}
+      on:consider={handleSort}
+      on:finalize={handleSort}
+    >
+      {#each items as item (item.id)}
+        <div>
+          <Card metric={item} />
         </div>
+      {/each}
     </div>
+  </div>
 </div>
-
