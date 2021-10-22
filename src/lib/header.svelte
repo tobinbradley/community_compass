7b48fbcc74f9b621be338a687878c3d79d53eb27
tobinbradley <tobin.bradley@gmail.com>
2021-10-22 17:35:46 -0400

    formatting

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,25 +1,24 @@
 <div class="bg-white shadow-xl">
-<div class="container mx-auto px-4">
-  <div
-    class="flex flex-col py-5 mx-auto md:items-center md:justify-between md:flex-row"
-  >
-    <div class="flex flex-row items-center justify-between lg:justify-start">
-      <a
-        href="./"
-        class="text-xl font-bold tracking-tighter text-blue-400 transition duration-500 ease-in-out transform tracking-relaxed pr-6"
-      >
-        Community Compass
-      </a>
-    </div>
-    <nav class="flex-grow  flex justify-end flex-row">
-      <ul
-        class="space-y-2 list-none lg:space-y-0 lg:items-center lg:inline-flex"
-      >
-        
-        <li>
-          <a
-            href="https://www.wickedtemplates.com/"
-            class="
+  <div class="container mx-auto px-4">
+    <div
+      class="flex flex-col py-5 mx-auto md:items-center md:justify-between md:flex-row"
+    >
+      <div class="flex flex-row items-center justify-between lg:justify-start">
+        <a
+          href="./"
+          class="text-xl font-bold tracking-tighter text-blue-400 transition duration-500 ease-in-out transform tracking-relaxed pr-6"
+        >
+          Community Compass
+        </a>
+      </div>
+      <nav class="flex-grow  flex justify-end flex-row">
+        <ul
+          class="space-y-2 list-none lg:space-y-0 lg:items-center lg:inline-flex"
+        >
+          <li>
+            <a
+              href="https://www.wickedtemplates.com/"
+              class="
           px-2
           lg:px-6
           py-6
@@ -31,12 +30,12 @@
           text-gray-400
           hover:text-blue-500
         "
-          >
-            Help
-          </a>
-        </li>
-      </ul>
-    </nav>
+            >
+              Help
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </div>
-</div>
 </div>
