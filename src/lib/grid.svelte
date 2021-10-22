4170d25b09c171375589759451bdb7df26c16eed
tobinbradley <tobin.bradley@gmail.com>
2021-10-22 17:32:09 -0400

    switch to sortablejs

--- src/lib/grid.svelte
+++ src/lib/grid.svelte
@@ -1,29 +1,15 @@
 <script>
-  import Card from "$lib/card.svelte";
-  import { dndzone } from "svelte-dnd-action";
-  import { activeMetrics } from "$lib/store";
+  import Card from "$lib/card.svelte"
+  import Sortable from '$lib/sortable.svelte'
+  import { activeMetrics } from "$lib/store"
 
-  $: items = $activeMetrics;
-
-  function handleSort(e) {
-    $activeMetrics = e.detail.items;
-    $activeMetrics = $activeMetrics;
-  }
+  $: items = $activeMetrics
 </script>
 
 <div class="flex-grow px-4 mt-8">
   <div class="container mx-auto">
-    <div
-      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
-      use:dndzone={{ items }}
-      on:consider={handleSort}
-      on:finalize={handleSort}
-    >
-      {#each items as item (item.id)}
-        <div>
-          <Card metric={item} />
-        </div>
-      {/each}
-    </div>
+    <Sortable {items} let:item={item}>
+      <Card metric={item} />
+    </Sortable>
   </div>
 </div>
