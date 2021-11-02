c475a4f30c4eee20ce359c3eafad0f34130ea237
tobinbradley <tobin.bradley@gmail.com>
2021-11-02 14:57:11 -0400

    cards to hash

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,5 +1,6 @@
 import { writable, readable, derived } from 'svelte/store'
 import dataConfig from '$lib/data/config.json'
+import { browser } from "$app/env"
 
 // assign ID for draggable grid
 function assignId(metric, idx) {
@@ -8,17 +9,40 @@ function assignId(metric, idx) {
   return newElem
 }
 
+// initial cards 
+let startCards = []
+if (browser) {
+  const args = window.location.hash.replace('#', '').split(',')
+  if (args[0].length > 0 && !args.some(isNaN)) {
+    startCards = []
+    args.forEach((arg, idx) => {
+      let metric = dataConfig.filter(el => el.metric == arg)
+      if (metric.length === 1) {
+        startCards.push(assignId(metric[0], idx + 1))
+      }
+    })
+  } else {
+    startCards = [
+      assignId(dataConfig.filter(el => el.metric == 39)[0], 1),
+      assignId(dataConfig.filter(el => el.metric == 54)[0], 2),
+      assignId(dataConfig.filter(el => el.metric == 20)[0], 3)
+    ]
+  }
+}
+
 // initial cards
-export let cards = writable([
-  assignId(dataConfig.filter(el => el.metric == 54)[0], 1),
-    assignId(dataConfig.filter(el => el.metric == 39)[0], 2),
-    assignId(dataConfig.filter(el => el.metric == 20)[0], 3),
-    assignId(dataConfig.filter(el => el.metric == 37)[0], 4)
-])
+export let cards = writable(startCards)
 
 // get max card ID to assign the next one
 export let maxId = derived(cards, $cards => {
   return Math.max(...$cards.map(el => el.id))
+})
+
+// set window hash
+cards.subscribe(value => {
+  if (browser) {
+    history.replaceState(null, null, `#${value.map(el => el.metric).join(',')}`)
+  }
 })
 
 // metric configuration
@@ -26,11 +50,6 @@ export let metrics = readable(dataConfig)
 
 // print styling toggle
 export let print = writable(false)
-
-export let modal = writable({
-  isOpen: false,
-  content: null
-})
 
 // print.subscribe(value => {
 //   console.log(value)
