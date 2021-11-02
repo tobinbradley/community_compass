78500a16a2e00ec81b57de5b7eb69affa1558a01
tobinbradley <tobin.bradley@gmail.com>
2021-11-02 16:24:43 -0400

    cleaner base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,13 @@
 import preprocess from "svelte-preprocess";
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.env.NODE_ENV == 'development'
-const base = dev ? '' : '/compass'
+const dev = process.env.NODE_ENV === 'development'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     paths: {
-      base: base
+      base: dev ? '' : '/compass'
     },
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
