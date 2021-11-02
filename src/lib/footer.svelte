c04a4a701e91ae79d8217c9b25cff2b1fbfe339f
tobinbradley <tobin.bradley@gmail.com>
2021-11-02 14:57:32 -0400

    rel tag to external links

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -4,15 +4,18 @@
       Created by <a href="https://gis.mecknc.gov">Mecklenburg County GIS</a> with much &#9829; for the projects that make this site possible:
     </div>
     <div class="links">
-      <a href="https://svelte.dev/" target="_blank">Svelte</a>,       <a href="https://leafletjs.com/" target="_blank">Leaflet</a>,       <a href="https://frappe.io/charts" target="_blank">Frappe Charts</a>, and <a href="https://tailwindcss.com/" target="_blank">TailwindCSS</a>.
+      <a href="https://svelte.dev/" rel="noopener" target="_blank">Svelte</a>, 
+      <a href="https://leafletjs.com/" rel="noopener" target="_blank">Leaflet</a>, 
+      <a href="https://frappe.io/charts" rel="noopener" target="_blank">Frappe Charts</a>, 
+      and <a href="https://tailwindcss.com/" rel="noopener" target="_blank">TailwindCSS</a>.
     </div>
     <div class="my-5">
-      <a href="https://twitter.com/MeckGIS" target="_blank" class="mr-2 transition-colors duration-300 ease-in-out hover:text-blue-500" title="Follow us on Twitter">
+      <a href="https://twitter.com/MeckGIS" rel="noopener" target="_blank" class="mr-2 transition-colors duration-300 ease-in-out hover:text-blue-500" title="Follow us on Twitter">
         <svg class="w-8 h-8 fill-current inline-block">
           <use xlink:href="#icon-twitter" />
         </svg>
       </a>
-      <a href="https://github.com/tobinbradley/community_compass" target="_blank" class="transition-colors duration-300 ease-in-out hover:text-blue-500" title="Source code on Github">
+      <a href="https://github.com/tobinbradley/community_compass" rel="noopener" target="_blank" class="transition-colors duration-300 ease-in-out hover:text-blue-500" title="Source code on Github">
         <svg class="w-8 h-8 fill-current inline-block">
           <use xlink:href="#icon-github" />
         </svg>
