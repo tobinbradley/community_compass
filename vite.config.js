5f8637e4cc68bde675d0143dc8961e3c9470b6fa
tobinbradley <tobin.bradley@gmail.com>
2022-10-05 10:37:52 -0400

    fix path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  base: './',
+  plugins: [svelte()],
+  server: {
+    port: 3000
+  }
 })
